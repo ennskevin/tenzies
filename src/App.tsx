4610726996dff1fa1cbf8f35661baf6ffd1d0466
ennskevin <ennskevin@tutanota.com>
2026-02-19 17:07:24 -0700

    Implement full functioning game

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,90 @@
+import { useState, useEffect } from "react"
+import { nanoid } from "nanoid"
+import Confetti from "react-confetti"
+
 import Die from "./components/Die"
 
 export default function App() {
 
+    const [diceObjects, setDiceObjects] = useState(generateAllNewDice)
+    const [heldDiceObjects, setHeldDiceObjects] = useState<any>([])
+    const [targetRoll, setTargetRoll] = useState<any>(null)
+
+    const gameWon = diceObjects.length === heldDiceObjects.length
+
+    const diceElements = diceObjects.map((o) => {
+        return <Die 
+            key={o.id}
+            id={o.id}
+            value={o.value}
+            isHeld={o.isHeld}
+            hold={hold} 
+        />
+    })
+
+    function generateAllNewDice() {
+        const arr = Array(10).fill({})
+        const newDiceObjects = arr.map(x => {
+            return {
+                value: getRandomValue(),
+                isHeld: false,
+                id: nanoid()
+            }
+        })
+        return newDiceObjects
+    }
+
+    function getRandomValue() {
+        return BigInt(Math.ceil(Math.random() * 6))
+    }
+
+    function startNewGame() {
+        setDiceObjects(generateAllNewDice)
+        setHeldDiceObjects([])
+        setTargetRoll(null)
+    }
+
+    function rollUnheldDice() {
+        setDiceObjects(prevDice => prevDice.map(die => {
+            if (die.isHeld === true) {
+                return die
+            }
+            else {
+                return {...die, value: getRandomValue()}
+            }
+        }))
+    }
+
+    function hold(id: string) {
+        const die = diceObjects.find(o => o.id === id)
+        const value = die?.value
+        if (targetRoll === null) {
+            setTargetRoll(value)
+        }
+        else if (value !== targetRoll) return
+        const newDiceObjects = diceObjects.map((o) => {
+            if (o.id === id) return {...o, isHeld: !o.isHeld}
+            return o
+        })
+        setDiceObjects(newDiceObjects)
+        const newHeldDiceObjects = newDiceObjects.filter(o => o.isHeld)
+        if (newHeldDiceObjects.length === 0) {
+            setTargetRoll(null)
+        }
+        setHeldDiceObjects(newHeldDiceObjects)
+    }
+
     return (
         <>
             <main>
+                {gameWon? <Confetti /> : null}
                 <div className="container main-section">
                     <span className="game-title">Tenzies</span>
-                    <span className="game-description">Roll until all dice are the same. Click each die to freeze it at its current value between rolls</span>
+                    <span className="game-description">Roll until all dice are the same. Click each die to hold it at its current value between rolls</span>
                     <div className="dice">
-                        <Die value={1n} />
-                        <Die value={1n} />
-                        <Die value={1n} />
-                        <Die value={1n} />
-                        <Die value={1n} />
-                        <Die value={1n} />
-                        <Die value={1n} />
-                        <Die value={1n} />
-                        <Die value={1n} />
-                        <Die value={1n} />
+                        {diceElements}
                     </div>
+                    <button className={"roll-button"} onClick={gameWon ? startNewGame : rollUnheldDice}>{gameWon ? "New Game" : "Roll"}</button>
                 </div>
             </main>
         </>
--- src/components/Die.tsx
+++ src/components/Die.tsx
@@ -1,11 +1,16 @@
+
 type Props = {
+    id: string;
     value: bigint;
+    isHeld: boolean;
+    hold: (id: string) => void;
 }
 
-export default function Die({ value }: Props) {
+export default function Die({ id, value, isHeld, hold }: Props) {
+
     return (
         <>
-            <button className="die">
+            <button className={`die ${isHeld ? "held" : ""}`} onClick={() => hold(id)}>
                 {value}
             </button>
         </>
